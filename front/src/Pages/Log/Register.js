fe6b6f6e1706a6237073d7993d09a1f8a6560320
Mong Diep <122360068+MongDiep94@users.noreply.github.com>
2023-12-01 10:35:48 +0100

    Register OK.

--- front/src/Pages/Log/Register.js
+++ front/src/Pages/Log/Register.js
@@ -28,17 +28,18 @@ const Register = () => {
     if (e.target.name === "lastName") {
       setLastName(e.target.value);
     }
+
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("firstName", firstName);
-    formData.append("lastName", lastName);
-    formData.append("email", email);
-    formData.append("password", password);
-
+    const userData = {
+      firstName,
+      lastName,
+      email,
+      password
+    }
 
     // Mettre les messages d'erreur à vide
     setEmailError("");
@@ -47,34 +48,48 @@ const Register = () => {
     // check pour les 2 inputs remplis
     if ("" === email) {
       setEmailError("Entrer votre addresse mail.");
+      console.log('email not empty');
       return;
     }
     // Regex vérif email
     if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i.test(email)) {
       setEmailError("Entrer une adresse mail valide, svp.");
+      console.log('email OK');
       return;
     }
 
     if ("" === password) {
       setPasswordError("Entrer un mot de passe.");
+      console.log('password not empty');
       return;
     }
 
     if (password.length < 7) {
       setPasswordError("Entrer un mot de passe de 8 caractères ou plus.");
+      console.log('password not OK');
       return;
     }
 
 
     axios
-      .post(`${process.env.REACT_APP_API}/register`, formData)
+      .post(`${process.env.REACT_APP_API}/register`, userData)
       .then((res) => {
-
         navigate("/se-connecter");
         return res.data;
       })
       .catch((error) => {
         console.error("Error registering user:", error);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error("Server responded with:", error.response.data);
+          console.error("Status code:", error.response.status);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.error("No response received from server");
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error("Error setting up the request:", error.message);
+        }
       });
 
   };
